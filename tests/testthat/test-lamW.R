65374a12a3d0b5909a423331326069b3924c1748
Avraham Adler <avraham.adler@gmail.com>
2020-05-21 19:00:17 -0400

    Updated tests from Fortran branch

--- tests/testthat/test-lamW.R
+++ tests/testthat/test-lamW.R
@@ -1,6 +1,6 @@
-PrincipleBranchAnswers <- runif(2500, min = -1, max = 703.227)
+PrincipleBranchAnswers <- runif(3000, min = -1, max = 703.227)
 PrincipleBranchTests <- PrincipleBranchAnswers * exp(PrincipleBranchAnswers)
-SecondaryBranchAnswers <- runif(2500, min = -703.227, max = -1)
+SecondaryBranchAnswers <- runif(3000, min = -703.227, max = -1)
 SecondaryBranchTests <- SecondaryBranchAnswers * exp(SecondaryBranchAnswers)
 
 context("Testing lambertW")
@@ -11,16 +11,25 @@ test_that("Functions return proper values", {
 })
 
 test_that("Function behaves properly near 0", {
-  V0 <- seq(-1e-2, 1e-2, 1e-6)
+  V0 <- seq(-2e-2, 2e-2, 2e-6)
   V0E <- V0 * exp(V0)
   LV0 <- lambertW0(V0E)
   expect_equal(V0, LV0)
-  Vm1 <- V0[V0 < 0]
-  LVm1 <- lambertWm1(Vm1)
-  expect_equal(Vm1, LVm1 * exp(LVm1))
 })
 
-test_that("Values outside domain properly trapped", {
+test_that("Function behaves properly near asymptotes", {
+  L <- seq(1e-6 - exp(-1), -0.25, 3e-6)
+  V0 <- lambertW0(L)
+  Vm1 <- lambertWm1(L)
+  expect_equal(V0 * exp(V0), L)
+  expect_equal(Vm1 * exp(Vm1), L)
+  Vm1 <- seq(-703, -703.227, -5e-6)
+  Vm1E <- Vm1 * exp(Vm1)
+  LVm1 <- lambertWm1(Vm1E)
+  expect_equal(Vm1, LVm1)
+})
+
+test_that("NaNs are returned for values outside domain", {
   expect_true(is.nan(lambertW0(-1)))
   expect_true(is.nan(lambertWm1(-1)))
   expect_equal(lambertWm1(0), -Inf)
