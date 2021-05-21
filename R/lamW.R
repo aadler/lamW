ef5fe9c60bd9642a5f46eb3c69dfeb43fc4364a4
Avraham Adler <avraham.adler@gmail.com>
2021-05-20 20:02:53 -0400

    Style consistency

--- R/lamW.R
+++ R/lamW.R
@@ -1,10 +1,10 @@
-lambertW0 <- function(x){
+lambertW0 <- function(x) {
   LAM <- double(length(x))
   LAM <- lambertW0_C(x)
   return(LAM)
 }
 
-lambertWm1 <- function(x){
+lambertWm1 <- function(x) {
   LAM <- double(length(x))
   LAM <- lambertWm1_C(x)
   return(LAM)
