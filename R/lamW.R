acf23246ab294451854f7264c207644a815276ad
Avraham Adler <avraham.adler@gmail.com>
2022-08-25 00:51:27 -0400

    This is unnecessary. We are not filling in a data structure piece by piece but creating it whole.

--- R/lamW.R
+++ R/lamW.R
@@ -1,11 +1,7 @@
 lambertW0 <- function(x) {
-  LAM <- double(length(x))
-  LAM <- lambertW0_C(x)
-  return(LAM)
+  lambertW0_C(x)
 }
 
 lambertWm1 <- function(x) {
-  LAM <- double(length(x))
-  LAM <- lambertWm1_C(x)
-  return(LAM)
+  lambertWm1_C(x)
 }
