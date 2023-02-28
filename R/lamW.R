3774fbb2568f70cc77a6c6620026f1dab2c74e13
Avraham Adler <avraham.adler@gmail.com>
2023-02-28 15:26:20 -0500

    Remove unnecessary newlines and add copyright/SPDX identifier

--- R/lamW.R
+++ R/lamW.R
@@ -1,7 +1,5 @@
-lambertW0 <- function(x) {
-  lambertW0_C(x)
-}
+# Copyright (c) 2015, Avraham Adler All rights reserved
+# SPDX-License-Identifier: BSD-2-Clause
+lambertW0 <- function(x) {lambertW0_C(x)}
 
-lambertWm1 <- function(x) {
-  lambertWm1_C(x)
-}
+lambertWm1 <- function(x) {lambertWm1_C(x)}
