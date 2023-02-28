bbd350cfc19825c37d6766c6b61e76cd710191fe
Avraham Adler <avraham.adler@gmail.com>
2023-02-28 15:29:36 -0500

    Update test code to be more consistent with personal style guide.

--- inst/tinytest/test_lamW.R
+++ inst/tinytest/test_lamW.R
@@ -1,45 +1,48 @@
+tol <- sqrt(.Machine$double.eps)
 # Test that functions return proper values
-PrincipleBranchAnswers <- runif(5000, min = -1, max = 703.22703310477016)
-PrincipleBranchTests <- PrincipleBranchAnswers * exp(PrincipleBranchAnswers)
-SecondaryBranchAnswers <- runif(5000, min = -714.96865723796657, max = -1)
-SecondaryBranchTests <- SecondaryBranchAnswers * exp(SecondaryBranchAnswers)
+principleBranchAnswers <- runif(5000, min = -1, max = 703.22703310477016)
+principleBranchTests <- principleBranchAnswers * exp(principleBranchAnswers)
+secondaryBranchAnswers <- runif(5000, min = -714.96865723796657, max = -1)
+secondaryBranchTests <- secondaryBranchAnswers * exp(secondaryBranchAnswers)
 
-expect_equal(lambertW0(PrincipleBranchTests), PrincipleBranchAnswers)
-expect_equal(lambertWm1(SecondaryBranchTests), SecondaryBranchAnswers)
+expect_equal(lambertW0(principleBranchTests), principleBranchAnswers,
+             tolerance = tol)
+expect_equal(lambertWm1(secondaryBranchTests), secondaryBranchAnswers,
+             tolerance = tol)
 
 # Test that function behaves properly near 0
 V0 <- seq(-2e-2, 2e-2, 2e-6)
 V0E <- V0 * exp(V0)
 LV0 <- lambertW0(V0E)
 
-expect_equal(V0, LV0)
+expect_equal(V0, LV0, tolerance = tol)
 
 # Test that W0 behaves properly VERY close to 0
-expect_equal(lambertW0(1e-275), 1e-275)
-expect_equal(lambertW0(7e-48), 7e-48)
-expect_equal(lambertW0(-3.81e-71), -3.81e-71)
+expect_identical(lambertW0(1e-275), 1e-275)
+expect_identical(lambertW0(7e-48), 7e-48)
+expect_identical(lambertW0(-3.81e-71), -3.81e-71)
 
 # Test that function behaves properly near -1/e
-expect_equal(lambertW0(-1/exp(1)), -1)
-expect_equal(lambertWm1(-1/exp(1)), -1)
+expect_identical(lambertW0(-1 / exp(1)), -1)
+expect_identical(lambertWm1(-1 / exp(1)), -1)
 
-# Test that function behaves properly near asymptotes
+# Test that function behaves properly near its asymptotes
 L <- seq(1e-6 - exp(-1), -0.25, 3e-6)
 V0 <- lambertW0(L)
-Vm1 <- lambertWm1(L)
+vm1 <- lambertWm1(L)
 
-expect_equal(V0 * exp(V0), L)
-expect_equal(Vm1 * exp(Vm1), L)
+expect_equal(V0 * exp(V0), L, tolerance = tol)
+expect_equal(vm1 * exp(vm1), L, tolerance = tol)
 
-Vm1 <- seq(-714, -714.96865, -3e-5)
-Vm1E <- Vm1 * exp(Vm1)
-LVm1 <- lambertWm1(Vm1E)
+vm1 <- seq(-714, -714.96865, -3e-5)
+vm1E <- vm1 * exp(vm1)
+lvm1 <- lambertWm1(vm1E)
 
-expect_equal(Vm1, LVm1)
+expect_equal(vm1, lvm1, tolerance = tol)
 
-# Test that function behaves properly at asymptotes
-expect_equal(lambertW0(Inf), Inf)
-expect_equal(lambertWm1(0), -Inf)
+# Test that function behaves properly at its asymptotes
+expect_identical(lambertW0(Inf), Inf)
+expect_identical(lambertWm1(0), -Inf)
 
 # Test that NaNs are returned for values outside domain
 expect_true(is.nan(lambertW0(-Inf)))
@@ -51,9 +54,9 @@ expect_true(is.nan(lambertWm1(1)))
 expect_true(is.nan(lambertW0(c(1, -1)))[[2]])
 
 # Test that integers are converted to reals for principle branch
-expect_equal(lambertW0(c(-1L, 0L, 1L, 2L, 3L, 4L)),
+expect_identical(lambertW0(c(-1L, 0L, 1L, 2L, 3L, 4L)),
              lambertW0(c(-1, 0, 1, 2, 3, 4)))
 
 ## Test CITATION
-expect_true(any(grepl(packageVersion('lamW'), toBibtex(citation('lamW')),
+expect_true(any(grepl(packageVersion("lamW"), toBibtex(citation("lamW")),
                       fixed = TRUE)))
