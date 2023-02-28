a7fda3c5e5a9d3fded7ac6c1801065175f76a2f0
Avraham Adler <avraham.adler@gmail.com>
2023-02-28 15:25:31 -0500

    Streamlined code to remove unnecessary variable declerations and implement a more logical ifelse cascade.

--- src/lambertW.cpp
+++ src/lambertW.cpp
@@ -55,8 +55,7 @@ const double M_1_E = 1.0 / M_E;
   * e_n = z_n / (1 + W_n) * (q_n - z_n) / (q_n - 2 * z_n)
   */
 
-double FritschIter(double x, double w_guess){
-  double w = w_guess;
+double FritschIter(double x, double w){
   int MaxEval = 5;
   bool CONVERGED = false;
   double k = 2.0 / 3.0;
@@ -75,64 +74,58 @@ double FritschIter(double x, double w_guess){
 }
 
 double lambertW0_CS(double x) {
-  double result;
-  double w;
   if (x == R_PosInf) {
-    result = R_PosInf;
+    return(R_PosInf);
   } else if (x < -M_1_E) {
-    result = R_NaN;
+    return(R_NaN);
   } else if (std::abs(x + M_1_E) <= EPS) {
-    result = -1.0;
-  } else if (x <= M_E - 0.5) {
-    if (std::abs(x) <= 1e-16) {
-      /* This close to 0 the W_0 branch is best estimated by its Taylor/Pade
-       expansion whose first term is the value x and remaining terms are below
-       machine double precision. See
-       https://math.stackexchange.com/questions/1700919
-      */
-      result = x;
-    } else {
-      if (std::abs(x) <= 7e-3) {
-        /* Use equation (5) in Fritsch */
-        w = ((1.33333333333333333 * x + 1.0) * x) /
-          ((0.83333333333333333 * x + 2.33333333333333333) * x + 1.0);
-      } else {
-        /* Use expansion in Corliss 4.22 to create (3, 2) Pade approximant
-        Numerator:-10189 / 303840 * p^3 + 40529 / 303840 * p^2 + 489 / 844 * p-1
-        Denominator: -14009 / 303840 * p^2 + 355 / 844 * p + 1
-        Converted to digits to reduce needed operations
-        */
-        double p = std::sqrt(2.0 * (M_E * x + 1.0));
-        double Numer = ((-0.03353409689310163 * p + 0.1333892838335966) * p +
-                        0.5793838862559242) * p - 1.0;
-        double Denom = (-0.04610650342285413 * p + 0.4206161137440758) * p + 1.0;
-        w = Numer / Denom;
-      }
-      result = FritschIter(x, w);
-    }
+    return(-1.0);
+  } else if (std::abs(x) <= 1e-16) {
+    /* This close to 0 the W_0 branch is best estimated by its Taylor/Pade
+     expansion whose first term is the value x and remaining terms are below
+     machine double precision. See
+     https://math.stackexchange.com/questions/1700919
+     */
+    return(x);
   } else {
-    /* Use first five terms of Corliss et al. 4.19 */
-    w = std::log(x);
-    double L_2 = std::log(w);
-    double L_3 = L_2 / w;
-    double L_3_sq = L_3 * L_3;
-    w += -L_2 + L_3 + 0.5 * L_3_sq - L_3 / w + L_3 / (w * w) - 1.5 * L_3_sq /
-      w + L_3_sq * L_3 / 3.0;
-    result = FritschIter(x, w);
+    double w;
+    if (std::abs(x) <= 6.4e-3) {
+      /* Use equation (5) in Fritsch */
+      w = ((1.33333333333333333 * x + 1.0) * x) /
+        ((0.83333333333333333 * x + 2.33333333333333333) * x + 1.0);
+    } else if (x <= M_E) {
+      /* Use expansion in Corliss 4.22 to create (3, 2) Pade approximant
+      Numerator:-10189 / 303840 * p^3 + 40529 / 303840 * p^2 + 489 / 844 * p-1
+      Denominator: -14009 / 303840 * p^2 + 355 / 844 * p + 1
+      Converted to digits to reduce needed operations
+      */
+      double p = std::sqrt(2.0 * (M_E * x + 1.0));
+      double Numer = ((-0.03353409689310163 * p + 0.1333892838335966) * p +
+                      0.5793838862559242) * p - 1.0;
+      double Denom = (-0.04610650342285413 * p + 0.4206161137440758) * p + 1.0;
+      w = Numer / Denom;
+    } else {
+      /* Use first five terms of Corliss et al. 4.19 */
+      w = std::log(x);
+      double L_2 = std::log(w);
+      double L_3 = L_2 / w;
+      double L_3_sq = L_3 * L_3;
+      w += -L_2 + L_3 + 0.5 * L_3_sq - L_3 / w + L_3 / (w * w) - 1.5 * L_3_sq /
+        w + L_3_sq * L_3 / 3.0;
+    }
+    return(FritschIter(x, w));
   }
-  return(result);
 }
 
 double lambertWm1_CS(double x){
-  double result;
-  double w;
   if (x == 0.0) {
-    result = R_NegInf;
+    return(R_NegInf);
   } else if (x < -M_1_E || x > 0.0) {
-    result = R_NaN;
+    return(R_NaN);
   } else if (std::abs(x + M_1_E) <= EPS) {
-    result = -1.0;
+    return(-1.0);
   } else {
+    double w;
     /* Use first five terms of Corliss et al. 4.19 */
     w = std::log(-x);
     double L_2 = std::log(-w);
@@ -140,9 +133,8 @@ double lambertWm1_CS(double x){
     double L_3_sq = L_3 * L_3;
     w += -L_2 + L_3 + 0.5 * L_3_sq - L_3 / w + L_3 / (w * w) - 1.5 * L_3_sq /
       w + L_3_sq * L_3 / 3.0;
-    result = FritschIter(x, w);
+    return(FritschIter(x, w));
   }
-  return(result);
 }
 
 struct LW0 : public Worker
