aa45062d048526c0d75867d610e649e4a8dfb556
Avraham Adler <avraham.adler@gmail.com>
2023-08-06 16:51:12 -0400

    Try this instead.

--- inst/tinytest/test_version.R
+++ inst/tinytest/test_version.R
@@ -8,8 +8,8 @@ expect_true(any(grepl(pV, toBibtex(citation("lamW")), fixed = TRUE)))
 
 # For some unknown reason this passes on Windows and Mac but not Ubuntu. Since
 # this is mainly for my own personal development purposes, I will wrap it in a
-# check for Windows.
-if (R.Version()$arch == "x86_64") {
+# check for "HOME".
+if (Sys.info()["nodename"] == "HOME") {
   # Test NEWS has most recent package version
   expect_true(any(grepl(pV, news(package = "lamW"), fixed = TRUE)))
 
